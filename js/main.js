9706a779686d9cd9814ffaeccc9c72d09666ac0f
vinaas <vinaas@email.com>
2019-09-17 12:57:04 +0700

    Tue 09/17/2019 12:56:55.06

--- js/main.js
+++ js/main.js
@@ -7,118 +7,81 @@ jQuery(function ($) {
     =             MAIN FUNCTION             =
     =======================================*/
     centralGroup.bannerSlider = function () {
-        if($('.slider__comp').length) {
-            $('.slider__comp').each(function() {
-                console.log('DATASET: ', this.dataset);
-                // dataset, có thể set giá trị mặc định nêu muốn - sử dụng ||
-                // data-slides-to-show="3", data-slides-to-scroll="1", data-dots="1", data-arrows="0"
-                let mSlidesToShow = parseInt(this.dataset.slidesToShow);
-                let mSlidesToScroll = parseInt(this.dataset.slidesToScroll);
-                let mDots = parseInt(this.dataset.dots);
-                let mArrows = parseInt(this.dataset.arrows);
-                let mAutoplay = parseInt(this.dataset.autoplay);
-                let mAutoplaySpeed = parseInt(this.dataset.autoplaySpeed);
-                let mResponsive = [];
-
-                console.log(mAutoplay);
-                console.log(mAutoplaySpeed);
-        
-                // Convert mDots, mArrows to True|False
-                mDots = !!mDots;
-                mArrows = !!mArrows;
-                mAutoplay = !!mAutoplay;
-                
-                if(mSlidesToShow === 4) {
-                    mResponsive = [
-                        {
-                            breakpoint: 1200,
-                            settings: {
-                                slidesToShow: 3,
-                                arrows: false
-                            }
-                        },
-                        {
-                            breakpoint: 992,
-                            settings: {
-                                slidesToShow: 2,
-                                arrows: false
-                            }
-                        },
-                        {
-                            breakpoint: 768,
-                            settings: {
-                                slidesToShow: 1,
-                                arrows: false
-                            }
-                        }
-                    ];
-                } else if (mSlidesToShow === 3) {
-                    mResponsive = [
-                        {
-                            breakpoint: 992,
-                            settings: {
-                                slidesToShow: 2,
-                                arrows: false
-                            }
-                        },
-                        {
-                            breakpoint: 768,
-                            settings: {
-                                slidesToShow: 1,
-                                arrows: false
-                            }
-                        }
-                    ];
-                } else if(mSlidesToShow === 2) {
-                    mResponsive = [
-                        {
-                            breakpoint: 768,
-                            settings: {
-                                slidesToShow: 1,
-                                arrows: false
-                            }
-                        }
-                    ];
-                }
-                console.log(mResponsive);
-        
-                $(this).slick({
-                    infinite: true,
-                    slidesToShow: mSlidesToShow,
-                    slidesToScroll: mSlidesToScroll,
-                    dots: mDots,
-                    arrows: mArrows,
-                    autoplay: mAutoplay,
-                    autoplaySpeed: mAutoplaySpeed,
-                    prevArrow: `<div class='prev'><i class='icon fas fa-chevron-left'></i></div>`,
-                    nextArrow: `<div class='next'><i class='icon fas fa-chevron-right'></i></div>`,
-                    responsive: mResponsive
-                });
+        if($('.banner__slider').length) {
+            $('.banner__slider').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 4000,
+                prevArrow: `<div class='prev'><i class='icon fas fa-chevron-left'></i></div>`,
+                nextArrow: `<div class='next'><i class='icon fas fa-chevron-right'></i></div>`,
             });
 
             $('.banner__slider').slickAnimation();
+        }
 
-            if($("#back-to-top").length) {
-                const valHeight = $(window).height();
-                
+
+        if($('.conduct__slider').length) {
+            $('.conduct__slider').slick({
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: true,
+                prevArrow: `<div class='prev'><i class='icon fas fa-chevron-left'></i></div>`,
+                nextArrow: `<div class='next'><i class='icon fas fa-chevron-right'></i></div>`,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3,
+                            arrows: false,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2,
+                            arrows: false,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            arrows: false,
+                            dots: true
+                        }
+                    }
+                ]
+            });
+        }
+
+        if($("#back-to-top").length) {
+            const valHeight = $(window).height();
+            
+            if($(window).scrollTop() < valHeight) {
+                $("#back-to-top").addClass('hidden');
+            } else {
+                $("#back-to-top").removeClass('hidden');
+            }
+            $(window).scroll(function() {
                 if($(window).scrollTop() < valHeight) {
                     $("#back-to-top").addClass('hidden');
                 } else {
                     $("#back-to-top").removeClass('hidden');
                 }
-                $(window).scroll(function() {
-                    if($(window).scrollTop() < valHeight) {
-                        $("#back-to-top").addClass('hidden');
-                    } else {
-                        $("#back-to-top").removeClass('hidden');
-                    }
-                });
-                $("#back-to-top").on('click', function() {
-                    $("html, body").animate({
-                        scrollTop: 0
-                    }, 1000);
-                });
-            }
+            });
+            $("#back-to-top").on('click', function() {
+                $("html, body").animate({
+                    scrollTop: 0
+                }, 1000);
+            });
         }
     };
 
